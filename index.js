66265e689e0351e0a4360387e278d190bc1fc2bc
BuildTools <sjuknelis@gmail.com>
2017-12-06 20:27:14 -0500

    Server works

---
+++ index.js
@@ -0,0 +1,17 @@
+var fs = require("fs");
+var express = require("express");
+var app = express();
+var server = require("http").Server(app);
+var io = require("socket.io")(server);
+var PORT = process.argv[2] || 8000;
+
+app.use("/scripts",express.static(__dirname + "/scripts"));
+
+io.on("connection",function(client) {
+  client.on("test",function(data) {
+    console.log(data);
+  });
+});
+
+server.listen(PORT);
+console.log("Listening on port " + PORT);

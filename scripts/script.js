7602e548b4920e9bd3eb7f3f852286ba6b07756c
BuildTools <sjuknelis@gmail.com>
2017-12-08 20:19:57 -0500

    Draw inventory

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,15 @@
 var ZOOM_SIZE = 50; // dependent on server
 var VISIBLE_SIZE = 40;
 var PLAYER_SIZE = 25;
+var loadedImages = {};
 var active = {
-  players: [],
-  powerups: [],
-  inventory: ["speed","speed","speed","health","health","test","test"],
+  players: [[100,100,"player1","#f00"],[90,90,"player2","#00f"]],
+  powerups: [[100,120,"speed"]],
+  inventory: [["speed",4],["health",3],["test",2]],
   health: 100
 };
 
-function renderAll(playerData,powerupData) {
+function renderAll(playerData,powerupData,inventoryData) {
   var canvas = document.getElementById("canvas");
   var size = Math.min(window.innerWidth,window.innerHeight)
   canvas.width = size;
@@ -45,18 +46,8 @@ function renderAll(playerData,powerupData) {
     ctx.font = "30px Arial";
     ctx.fillText(playerData[i][2],dx,dy - 40);
   }
-  // render sidebar
-  ctx.strokeStyle = "#ffffff";
-  for ( var i = 0; i < 10; i++ ) {
-    if ( i == 0 ) ctx.fillStyle = "#000099";
-    else if ( i == 1 ) ctx.fillStyle = "#990000";
-    else ctx.fillStyle = "#000000";
-    var dy = (i * ZOOM_SIZE) + (size - 10 * ZOOM_SIZE);
-    ctx.fillRect(0,dy,ZOOM_SIZE,ZOOM_SIZE);
-    ctx.strokeRect(0,dy,ZOOM_SIZE,ZOOM_SIZE);
-
-  }
-  // render powerups (has to be last because loading)
+  // render powerups
+  var loadedCount = 0;
   for ( var i = 0; i < powerupData.length; i++ ) {
     var dx = powerupData[i][0] - playerData[0][0];
     var dy = (powerupData[i][1] - 1) - playerData[0][1];
@@ -65,14 +56,53 @@ function renderAll(playerData,powerupData) {
     dy *= size / ZOOM_SIZE;
     dx += size / 2;
     dy += size / 2;
-    var img = new Image();
-    img.src = "/imageassets/powerup_" + powerupData[i][2] + ".png";
-    img.onload = function() {
+    getImage("/imageassets/powerup_" + powerupData[i][2] + ".png",function(img) {
       ctx.drawImage(img,dx,dy);
+      loadedCount++;
+    });
+  }
+  var interval = setInterval(function() {
+    if ( loadedCount >= powerupData.length ) {
+      clearInterval(interval);
+      // render inventory
+      var loadedCount = 0;
+      ctx.strokeStyle = "#ffffff";
+      ctx.font = "30px Arial";
+      ctx.fillStyle = "#000000";
+      for ( var i = 0; i < 10; i++ ) {
+        var dy = (i * ZOOM_SIZE) + (size - 10 * ZOOM_SIZE) - 2;
+        ctx.fillRect(0,dy,ZOOM_SIZE,ZOOM_SIZE);
+        ctx.strokeRect(0,dy,ZOOM_SIZE,ZOOM_SIZE);
+        if ( inventoryData[i] ) {
+          getImage("/imageassets/powerup_" + inventoryData[i][0] + ".png",[dy,inventoryData[i][1]],function(img,value) {
+            ctx.drawImage(img,2,value[0] + 2);
+            ctx.fillText(value[1],16,value[0] + 35);
+            loadedCount++;
+          });
+        }
+      }
+      
+    }
+  },100);
+}
+
+function getImage(url,secure,callback) {
+  if ( ! callback ) {
+    callback = secure;
+    secure = null;
+  }
+  if ( loadedImages[url] ) {
+    callback(loadedImages[url],secure);
+  } else {
+    var img = new Image();
+    img.src = url;
+    img.onload = function() {
+      loadedImages[url] = img;
+      callback(img,secure);
     }
   }
 }
 
 window.onload = function() {
-  renderAll([[100,100,"player1","#f00"],[90,90,"player2","#00f"]],[[100,120,"speed"]]);
+  renderAll(active.players,active.powerups,active.inventory);
 }
